e549a0e01f457e9681319fea7ff49101da288db6
IBM <IBM@IBM-Testing01>
2020-02-12 17:15:48 +0530

    First file in Repository-simple level test cases

---
+++ tests/com/training/sanity/tests/Filter.java
@@ -0,0 +1,76 @@
+package com.training.sanity.tests;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.training.generics.ScreenShot;
+import com.training.pom.LoginPOM;
+import com.training.utility.DriverFactory;
+import com.training.utility.DriverNames;
+
+public class Filter {
+	private WebDriver driver;
+	private String baseUrl;
+	private LoginPOM loginPOM;
+	private static Properties properties;
+	private ScreenShot screenShot;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws IOException {
+		properties = new Properties();
+		FileInputStream inStream = new FileInputStream("./resources/others.properties");
+		properties.load(inStream);
+	}
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		driver = DriverFactory.getDriver(DriverNames.CHROME);
+		loginPOM = new LoginPOM(driver); 
+		baseUrl = properties.getProperty("baseURL");
+		screenShot = new ScreenShot(driver); 
+		// open the browser 
+		driver.get(baseUrl);
+	}
+	
+	@AfterMethod
+	public void tearDown() throws Exception {
+		Thread.sleep(1000);
+		driver.quit();
+	}
+	@Test
+	public void validLoginTest() throws InterruptedException {
+		loginPOM.sendUserName("samsaravanan");
+		loginPOM.But1();
+		loginPOM.But2();
+		loginPOM.But3();
+		loginPOM.But4();
+		loginPOM.submit();
+		loginPOM.perso();
+		loginPOM.mess();
+		loginPOM.advance();
+		loginPOM.sele();
+		loginPOM.sele1();
+		loginPOM.sees();
+		loginPOM.selen();
+		loginPOM.selen1();
+		loginPOM.selenw();
+		loginPOM.swa();
+		loginPOM.swa1();
+		loginPOM.swa2();
+		
+		
+       
+	}
+	
+	
+	
+}
+
